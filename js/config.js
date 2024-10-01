180a95c072bebb5013ddca789850b1535257bbe4
Munal Dhakal <dhakalmunal@gmail.com>
2024-10-01 14:53:14 -0400

    Updated to use config

---
+++ js/config.js
@@ -0,0 +1,22 @@
+// config.js
+const API_CONFIG = {
+    BASE_URL: 'http://localhost:3000',
+    ENDPOINTS: {
+        PRODUCTS: '/api/products',
+        CATEGORIES: '/api/categories',
+        SUBCATEGORIES: '/api/subcategories',
+        LOGIN: '/api/login',
+    },
+    getFullUrl: function(endpoint) {
+        return this.BASE_URL + this.ENDPOINTS[endpoint];
+    },
+    getHeaders: function() {
+        return {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+            'Content-Type': 'application/json'
+        };
+    }
+};
+
+// Prevent modifications to the config object
+Object.freeze(API_CONFIG);
