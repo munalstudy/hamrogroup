08afa59c95768637f6be5ef0176b7c22b1cdb461
Munal Dhakal <dhakalmunal@gmail.com>
2024-10-01 15:06:34 -0400

    Updated files to use the config

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ const API_CONFIG = {
     getHeaders: function() {
         return {
             'Authorization': `Bearer ${localStorage.getItem('token')}`,
-            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/json'
         };
     }
 };
